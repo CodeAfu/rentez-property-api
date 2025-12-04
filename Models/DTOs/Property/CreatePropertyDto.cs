9c49b35b673a4276b8d6cf4bed7ee30ae93f4047
CodeAfu <ibrahim.afzal1999@gmail.com>
2025-12-04 11:11:59 +0800

    made create property params nullable

--- Models/DTOs/Property/CreatePropertyDto.cs
+++ Models/DTOs/Property/CreatePropertyDto.cs
@@ -4,12 +4,12 @@ using RentEZApi.Models.DTOs.Property;
 public class CreatePropertyDto
 {
     public required string Title { get; set; }
-    public required string Description { get; set; }
-    public required string Address { get; set; }
-    public required string City { get; set; }
-    public required string State { get; set; }
-    public required decimal Rent { get; set; }
-    public required string[] Images { get; set; } = Array.Empty<string>();
+    public string Description { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public decimal Rent { get; set; }
+    public string[] Images { get; set; } = Array.Empty<string>();
     public bool? DepositRequired { get; set; }
     public BillsIncluded? BillsIncluded { get; set; } = new();
     public Guid? LeaseAgreementId { get; set; }
